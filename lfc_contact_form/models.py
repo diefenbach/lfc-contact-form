fc47d0cdf258dd384ea989c8d998b928ed2a27c2
Kai Diefenbach <kai.diefenbach@iqpp.de>
2014-05-10 10:22:37 +0200

    Replaces RichTextField with TextField from contact manage form.

--- lfc_contact_form/models.py
+++ lfc_contact_form/models.py
@@ -10,7 +10,7 @@ from django.utils.translation import ugettext_lazy as _
 
 # lfc imports
 import lfc.utils
-from lfc.fields.rich_text import RichTextField
+from lfc.fields.wysiwyg import WYSIWYGInput
 from lfc.models import BaseContent
 
 # lfc_contact_form imports
@@ -20,13 +20,13 @@ from lfc_contact_form.forms import ContactForm as DjangoContactForm
 class ContactForm(BaseContent):
     """Contact form for LFC.
     """
-    text = RichTextField(_(u"Text"), blank=True)
+    text = models.TextField(_(u"Text"), blank=True)
     thank_you_message = models.TextField(blank=True)
 
     def get_searchable_text(self):
         """Returns the searchable text of the contact form.
         """
-        searchable_text = self.title + " " + self.description + " " + self.text.text
+        searchable_text = self.title + " " + self.description + " " + self.text
         return lfc.utils.html2text(searchable_text)
 
     def edit_form(self, **kwargs):
@@ -64,19 +64,15 @@ class ContactForm(BaseContent):
 
         return super(ContactForm, self).render(request)
 
-    def has_seo_tab(self):
-        return super(ContactForm, self).has_seo_tab(False)
-
-    def has_comments_tab(self):
-        return super(ContactForm, self).has_comments_tab(False)
-
-    def has_children_tab(self):
-        return super(ContactForm, self).has_children_tab(False)
-
 
 class ContactFormForm(forms.ModelForm):
     """The add/edit form of the ContactForm content object.
     """
     class Meta:
         model = ContactForm
-        fields = ("title", "display_title", "slug", "description", "text_type", "text", "thank_you_message")
+        fields = ("title", "display_title", "slug", "description", "text", "thank_you_message")
+
+    def __init__(self, *args, **kwargs):
+        super(ContactFormForm, self).__init__(*args, **kwargs)
+        self.fields["text"].widget = WYSIWYGInput()
+        self.fields["thank_you_message"].widget = WYSIWYGInput()
